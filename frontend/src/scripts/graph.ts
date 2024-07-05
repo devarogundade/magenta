c6ba51474dc8c57d6b88a08ce77f27c493e81edd
devarogundade <devarogundade@gmail.com>
2024-07-05 01:45:50 +0100

    update graph api

--- frontend/src/scripts/graph.ts
+++ frontend/src/scripts/graph.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { DCAOrderCreated, LimitOrderCreated, SwapOrderCreated } from "@/types";
 
-const Endpoint: string = 'https://api.goldsky.com/api/public/project_cly656z1bbfza0114c0f02goy/subgraphs/magenta/2.0/g';
+const Endpoint: string = 'https://api.goldsky.com/api/public/project_cly656z1bbfza0114c0f02goy/subgraphs/magenta/2.0/gn';
 
 export async function getSwapOrders(address: `0x${string}`, first = 15): Promise<SwapOrderCreated[]> {
     try {
